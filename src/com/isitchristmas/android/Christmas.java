9f66976da8978411ceed6832abeb106c8a698e66
klondike <kprojection@gmail.com>
2009-11-24 12:28:17 -0500

    Some preliminary code for dealing with locales

--- src/com/isitchristmas/android/Christmas.java
+++ src/com/isitchristmas/android/Christmas.java
@@ -1,20 +1,46 @@
 package com.isitchristmas.android;
 
+import java.util.Locale;
+
 import android.text.format.Time;
 
 public class Christmas {
 
+	public static String answer(boolean isIt, Locale locale) {
+		return isIt ? yes(locale) : no(locale); 
+	}
+	
 	public static boolean isIt() {
 		Time now = new Time();
 		now.set(System.currentTimeMillis());
-		return isIt(now);
-	}
-	
-	public static boolean isIt(Time time) {
 		return false;
 	}
 	
-	public static String yes() {
+	public static String yes(Locale locale) {
+		String country = locale.getCountry();
+		String language = locale.getLanguage();
+		String code = locale.toString();
+		
+		if (country.equals("CA"))
+			return "YES/OUI";
+		
+		if (language.equals("en"))
+			return "YES"; 
+							
 		return "YES";
+	}
+	
+	public static String no(Locale locale) {
+		String country = locale.getCountry();
+		String language = locale.getLanguage();
+		String code = locale.toString();
+		
+		if (country.equals("CA"))
+			return "NO/NON";
+		
+		if (language.equals("en"))
+			return "NO";
+		
+		return "NO";
 	}
 }
--- src/com/isitchristmas/android/ChristmasView.java
+++ src/com/isitchristmas/android/ChristmasView.java
@@ -1,7 +1,10 @@
 package com.isitchristmas.android;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChristmasView extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,11 @@ public class ChristmasView extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        isItChristmas();
+    }
+    
+    public void isItChristmas() {
+    	((TextView) findViewById(R.id.answer)).setText(Christmas.answer(Christmas.isIt(), Locale.getDefault()));
     }
 }
