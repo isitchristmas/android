a5344ed184403d24bc0255928ec3ae0e28f473e4
klondike <kprojection@gmail.com>
2009-11-26 17:36:10 -0500

    Fully internationalized Is It Christmas using the locales reported by the Android 1.6 emulator

--- src/com/isitchristmas/android/Christmas.java
+++ src/com/isitchristmas/android/Christmas.java
@@ -6,41 +6,147 @@ import android.text.format.Time;
 
 public class Christmas {
 
-	public static String answer(boolean isIt, Locale locale) {
+	public static int answer(boolean isIt, Locale locale) {
 		return isIt ? yes(locale) : no(locale); 
 	}
 	
 	public static boolean isIt() {
 		Time now = new Time();
 		now.set(System.currentTimeMillis());
-		return false;
+		return now.month == 11 && now.monthDay == 25;
 	}
 	
-	public static String yes(Locale locale) {
+	public static int yes(Locale locale) {
 		String country = locale.getCountry();
 		String language = locale.getLanguage();
-		String code = locale.toString();
 		
 		if (country.equals("CA"))
-			return "YES/OUI";
+			return R.string.canada_yes;
 		
+		if (language.equals("ar"))
+			return R.string.ar_yes;
+		if (language.equals("cs"))
+			return R.string.cs_yes;
+		if (language.equals("da"))
+			return R.string.da_yes;
+		if (language.equals("de"))
+			return R.string.de_yes;
+		if (language.equals("el"))
+			return R.string.el_yes;
 		if (language.equals("en"))
-			return "YES"; 
+			return R.string.en_yes;
+		if (language.equals("es"))
+			return R.string.es_yes;
+		if (language.equals("fi"))
+			return R.string.fi_yes;
+		if (language.equals("fr"))
+			return R.string.fr_yes;
+		if (language.equals("iw"))
+			return R.string.iw_yes;
+		if (language.equals("hr"))
+			return R.string.hr_yes;
+		if (language.equals("hu"))
+			return R.string.hu_yes;
+		if (language.equals("in"))
+			return R.string.in_yes;
+		if (language.equals("it"))
+			return R.string.it_yes;
+		if (language.equals("ko"))
+			return R.string.ko_yes;
+		if (language.equals("nb"))
+			return R.string.nb_yes;
+		if (language.equals("nl"))
+			return R.string.nl_yes;
+		if (language.equals("pl"))
+			return R.string.pl_yes;
+		if (language.equals("pt"))
+			return R.string.pt_yes;
+		if (language.equals("ro"))
+			return R.string.ro_yes;
+		if (language.equals("ru"))
+			return R.string.ru_yes;
+		if (language.equals("sk"))
+			return R.string.sk_yes;
+		if (language.equals("sv"))
+			return R.string.sv_yes;
+		if (language.equals("th"))
+			return R.string.th_yes;
+		if (language.equals("tr"))
+			return R.string.tr_yes;
+		if (language.equals("uk"))
+			return R.string.uk_yes;
+		if (language.equals("zh"))
+			return R.string.zh_yes;
+		if (language.equals("sr"))
+			return R.string.sr_yes;
 							
-		return "YES";
+		return R.string.default_yes;
 	}
 	
-	public static String no(Locale locale) {
+	public static int no(Locale locale) {
 		String country = locale.getCountry();
 		String language = locale.getLanguage();
-		String code = locale.toString();
 		
 		if (country.equals("CA"))
-			return "NO/NON";
+			return R.string.canada_no;
 		
+		if (language.equals("ar"))
+			return R.string.ar_no;
+		if (language.equals("cs"))
+			return R.string.cs_no;
+		if (language.equals("da"))
+			return R.string.da_no;
+		if (language.equals("de"))
+			return R.string.de_no;
+		if (language.equals("el"))
+			return R.string.el_no;
 		if (language.equals("en"))
-			return "NO";
+			return R.string.en_no;
+		if (language.equals("es"))
+			return R.string.es_no;
+		if (language.equals("fi"))
+			return R.string.fi_no;
+		if (language.equals("fr"))
+			return R.string.fr_no;
+		if (language.equals("iw"))
+			return R.string.iw_no;
+		if (language.equals("hr"))
+			return R.string.hr_no;
+		if (language.equals("hu"))
+			return R.string.hu_no;
+		if (language.equals("in"))
+			return R.string.in_no;
+		if (language.equals("it"))
+			return R.string.it_no;
+		if (language.equals("ko"))
+			return R.string.ko_no;
+		if (language.equals("nb"))
+			return R.string.nb_no;
+		if (language.equals("nl"))
+			return R.string.nl_no;
+		if (language.equals("pl"))
+			return R.string.pl_no;
+		if (language.equals("pt"))
+			return R.string.pt_no;
+		if (language.equals("ro"))
+			return R.string.ro_no;
+		if (language.equals("ru"))
+			return R.string.ru_no;
+		if (language.equals("sk"))
+			return R.string.sk_no;
+		if (language.equals("sv"))
+			return R.string.sv_no;
+		if (language.equals("th"))
+			return R.string.th_no;
+		if (language.equals("tr"))
+			return R.string.tr_no;
+		if (language.equals("uk"))
+			return R.string.uk_no;
+		if (language.equals("zh"))
+			return R.string.zh_no;
+		if (language.equals("sr"))
+			return R.string.sr_no;
 		
-		return "NO";
+		return R.string.default_no;
 	}
 }
