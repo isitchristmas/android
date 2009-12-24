436f4f6528bb988a04268c3caec65c731e381689
klondike <kprojection@gmail.com>
2009-12-24 02:59:37 -0500

    Shuffled icons

--- src/com/isitchristmas/android/ChristmasWidgetProvider.java
+++ src/com/isitchristmas/android/ChristmasWidgetProvider.java
@@ -17,14 +17,18 @@ public class ChristmasWidgetProvider extends AppWidgetProvider {
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-            
-            int answerId = Christmas.answer(Christmas.isIt(), Locale.getDefault());
-            String answer = context.getResources().getString(answerId);
-            views.setTextViewText(R.id.answer, answer);
+            RemoteViews views = buildView(context);
             
             manager.updateAppWidget(appWidgetId, views);
         }
 	}
 	
+	public static RemoteViews buildView(Context context) {
+		int answerId = Christmas.answer(Christmas.isIt(), Locale.getDefault());
+        String answer = context.getResources().getString(answerId);
+        
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+        views.setTextViewText(R.id.answer, answer);
+        return views;
+	}
 }
